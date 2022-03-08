b712b5685ea01895dde3020f95e71269b2a51c58
Nithya <nithya.vinod21@gmail.com>
2022-03-08 22:41:51 +0530

    Fist Commit

---
+++ src/component/FirstComponent.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+
+class FirstComponent extends Component{
+    render(){
+        return(
+            <> 
+            <h1> My First Class Component </h1>
+            <p> virtual dom</p>
+            </>   
+        )
+
+       
+    }
+}
+
+export default (FirstComponent);
